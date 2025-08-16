c72d1162c10e967864b33e9d08bb0f148b270c02
Marilu <flaminaschannel@gmail.com>
2025-08-16 12:41:21 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const { readFileSync } = require('fs');
 
 function gerarFaturaStr(fatura, pecas) {
-  // função auxiliar pedida na etapa 1
-  function calcularTotalApresentacao(apre, peca) {
+  function getPeca(apresentacao) {
+    return pecas[apresentacao.id];
+  }
+
+  function calcularTotalApresentacao(apre) {
+    const peca = getPeca(apre); // busca a peça aqui dentro
     let total = 0;
     switch (peca.tipo) {
       case "tragedia":
@@ -20,20 +24,18 @@ function gerarFaturaStr(fatura, pecas) {
     return total;
   }
 
-  // aqui montamos a string de saída
   let faturaStr = `Fatura ${fatura.cliente}\n`;
   for (let apre of fatura.apresentacoes) {
-    const peca = pecas[apre.id];
-    faturaStr += `  ${peca.nome}: R$ ${(calcularTotalApresentacao(apre, peca)/100).toFixed(2)} (${apre.audiencia} assentos)\n`;
+    const pecaNome = getPeca(apre).nome; // usa a query diretamente
+    const valor = calcularTotalApresentacao(apre); // sem "peca" como argumento
+    faturaStr += `  ${pecaNome}: R$ ${(valor / 100).toFixed(2)} (${apre.audiencia} assentos)\n`;
   }
 
   return faturaStr;
 }
 
-// leitura dos arquivos JSON
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas   = JSON.parse(readFileSync('./pecas.json'));
 
-// gera e imprime
 const faturaStr = gerarFaturaStr(faturas, pecas);
 console.log(faturaStr);
